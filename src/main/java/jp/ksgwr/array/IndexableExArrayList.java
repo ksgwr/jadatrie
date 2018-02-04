ab6b9f77e1e21a8659eded600eb343729163db13
ksgwr <ksgwarai@gmail.com>
2018-02-04 18:14:05 +0900

    remove unuse Interface

--- src/main/java/jp/ksgwr/array/IndexableExArrayList.java
+++ src/main/java/jp/ksgwr/array/IndexableExArrayList.java
@@ -9,7 +9,20 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public interface IndexableExArrayList<E extends Serializable> extends Indexable<E>, ExArrayList<E> {
+public interface IndexableExArrayList<E extends Serializable> extends ExArrayList<E> {
 
+    /**
+     * load external index
+     * @param index index
+     * @throws IOException file error
+     * @throws ClassNotFoundException target class error
+     */
+    void load(SeparableIndex<E> index) throws IOException, ClassNotFoundException;
 
+    /**
+     * save index
+     * @param index index
+     * @throws IOException file error
+     */
+    void save(SeparableIndex<E> index) throws IOException;
 }
